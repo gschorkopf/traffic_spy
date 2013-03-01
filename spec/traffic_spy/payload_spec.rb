842c2d2c592c02648819a36b52c171526367e711
Geoff Schorkopf <gschorkopf@gmail.com>
2013-02-28 21:13:52 -0700

    created payloads files

---
+++ spec/traffic_spy/payload_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe TrafficSpy::Payload do
+
+  let(:app) { TrafficSpy::Payload }
+
+  describe "initialize stores variables" do
+    it "stores a hash of data" do
+      pending "Unbuilt function"
+    end
+  end
+
+end
