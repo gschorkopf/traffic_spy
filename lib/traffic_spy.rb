17334568e17608e688ace498e00672c202bf89aa
Bradley Sheehan <bradpsheehan@gmail.com>
2013-03-04 21:13:01 -0700

    URL Spec Page shows response times

--- lib/traffic_spy.rb
+++ lib/traffic_spy.rb
@@ -55,10 +55,9 @@ module TrafficSpy
       @source = Client.data.where(identifier: params[:identifier]).to_a.first[:identifier]
       # raise @source.inspect
 
-      # if @source.count == 0
-      #   status 400
-      #   erb :error
-      # else
+      if @source == ""
+        erb :error_400
+      else
         client_id = Client.data.where(identifier: params[:identifier]).to_a.first[:id]
         @rooturl = Client.data.where(identifier: params[:identifier]).to_a.first[:rooturl]
         payloads_to_use = Payload.find_all_by_client_id(client_id)
@@ -72,43 +71,37 @@ module TrafficSpy
         @screen_rezs = Payload.rez_sorter(payloads_to_use)
         @response_times = Payload.avg_response_times(payloads_to_use)
         @url_spec_metrics = Payload.url_sorter(payloads_to_use).to_a
-        # @agg_event_data =
-
         erb :app_details_index
-      # end
+
+        @agg_event_data = 
+
+      end
 
     end
 
   get "/sources/:identifier/urls/:path" do
     @identifier = params[:identifier]
     @relative_path = params[:path]
-    # raise @path.inspect
 
     @source = Client.data.where(identifier: @identifier).to_a[0]
-    # raise @source.inspect
 
-    # @rooturl = @source[:rooturl]
-    # raise @rooturl.inspect
-
-    # @relative_path = @path.gsub(@rooturl, '')
-    # raise @relative_path.inspect
-
-    # if @source.count == 0 && @relative_path.count == 0
-    #   status 400
-    #   "{\"message\":\"No url for identifier.\"}"
-
-    # LOOKUP IN PAYLOADS TABLE FOR PATH RESPONSE TIMES
-    # else
-      payloads_to_use = Payload.find_all_by_path(params[:path])
+    if @source == nil || @relative_path == nil
+      erb :error
+    else
+      path = "/#{@relative_path}"
+      payloads_to_use = Payload.find_all_by_path(path)
       @response_times = Payload.response_times_for_path(payloads_to_use)
+
       erb :url_stats
-    # end
+    end
   end
 
-    # get "/sources/:identifier/:events" do
+    # get "/sources/:identifier/events/:name" do
+    # @identifier = params[:identifier]
+    # @source = Client.data.where(identifier: @identifier).to_a.first[:identifier]
+    # @name = params[:name]
 
     # if #no events have been defined
-    #   status 400
     #   "{\"message\":\"No events have been defined.\"}"
     # else
     #   payloads_to_use = Payload.find_all_by_path(path)
