fe3d0a33af6aa17e6587a0f1988d3a8eab8ec387
Geoff Schorkopf <gschorkopf@gmail.com>
2013-03-05 11:16:41 -0700

    adding tables to db

--- db/migrations/001_create_table.rb
+++ db/migrations/001_create_table.rb
@@ -5,10 +5,31 @@ Sequel.migration do
     #   # String        :field_1
     #   # Text          :field_2
     # end
-    create_table(:identifiers) do
+    create_table :identifiers do
       primary_key :id
       String      :identifier
       String      :rooturl
+    end
+
+    create_table :payloads do
+      primary_key :id
+      foreign_key :client_id
+      foreign_key :event_id
+      String      :user_agent
+      String      :url
+      String      :path
+      DateTime    :requested_at
+      Integer     :responded_in
+      String      :referred_by
+      String      :request_type
+      String      :resolution_width
+      String      :resolution_height
+      String      :ip
+    end
+
+    create_table :campaigns do
+      primary_key :id
+      String      :name
     end
   end
 end
